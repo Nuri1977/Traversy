6b9aa78225287eb9fd2bee58626c6f8f7a1d5e1c
nurilacka-admin <nuri@adaptivescale.com>
2023-06-21 12:39:56 +0200

    register, login, profile info and update

--- backend/controllers/userController.ts
+++ backend/controllers/userController.ts
@@ -5,10 +5,41 @@ import asyncHandler from "express-async-handler";
 //@route POST /api/users/auth
 //@access Public
 const authUser = asyncHandler(async (req: Request, res: Response) => {
-  res.status(401);
-  throw new Error("Something went wrong");
-
   res.status(200).json({ message: "Auth user" });
 });
 
-export { authUser };
+//@des   Register a new user
+//@route POST /api/users
+//@access Public
+const registerUser = asyncHandler(async (req: Request, res: Response) => {
+  res.status(200).json({ message: "Register user" });
+});
+
+//@des   Logout a user
+//@route POST /api/users/logout
+//@access Public
+const logoutUser = asyncHandler(async (req: Request, res: Response) => {
+  res.status(200).json({ message: "Logout  user" });
+});
+
+//@des   Get user profile
+//@route GET /api/users/profile
+//@access Private
+const getUserProfile = asyncHandler(async (req: Request, res: Response) => {
+  res.status(200).json({ message: "Get User profile" });
+});
+
+//@des   Update user profile
+//@route PUT /api/users/profile
+//@access Private
+const updateUserProffile = asyncHandler(async (req: Request, res: Response) => {
+  res.status(200).json({ message: "Update User Proffile" });
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProffile,
+};
