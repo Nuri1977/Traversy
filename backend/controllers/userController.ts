9a9d8eeaaa71761121b2e261191ce49395e4828f
nurilacka-admin <nuri@adaptivescale.com>
2023-06-21 12:23:38 +0200

    MiddleWare for notFound and Errors

--- backend/controllers/userController.ts
+++ backend/controllers/userController.ts
@@ -1,11 +1,14 @@
-import exp from "constants";
 import { Request, Response } from "express";
+import asyncHandler from "express-async-handler";
 
 //@des   Auth user & get token
 //@route POST /api/users/auth
 //@access Public
-const authUser = async (req: Request, res: Response) => {
+const authUser = asyncHandler(async (req: Request, res: Response) => {
+  res.status(401);
+  throw new Error("Something went wrong");
+
   res.status(200).json({ message: "Auth user" });
-};
+});
 
 export { authUser };
--- backend/index.ts
+++ backend/index.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import userRoutes from "./routes/userRoutes.js";
 dotenv.config();
 const port = process.env.PORT || 5000;
-import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 
 app.use("/api/users", userRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 app.get("/", (req, res) => {
   res.send("Server is ready");
