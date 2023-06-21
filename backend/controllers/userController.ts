7cc186dc2c8172a2fd9c57d9441318aad8125cba
nurilacka-admin <nuri@adaptivescale.com>
2023-06-21 17:12:52 +0200

    Auth user endpoint

--- backend/controllers/userController.ts
+++ backend/controllers/userController.ts
@@ -7,7 +7,15 @@ import User from "../models/userModel.js";
 //@route POST /api/users/auth
 //@access Public
 const authUser = asyncHandler(async (req: Request, res: Response) => {
-  res.status(200).json({ message: "Auth user" });
+  const { email, password } = req.body;
+  const user = (await User.findOne({ email })) as any;
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id.toString());
+    res.status(201).json({ _id: user._id, name: user.name, email: user.email });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
 });
 
 //@des   Register a new user
@@ -20,7 +28,6 @@ const registerUser = asyncHandler(async (req: Request, res: Response) => {
     res.status(400);
     throw new Error("User already exists");
   }
-
   const user = await User.create({ name, email, password });
   if (user) {
     generateToken(res, user._id.toString());
